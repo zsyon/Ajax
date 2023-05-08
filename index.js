898fc4f98f6e899d9e4f4204594bb9b8608bdea0
HP <20060339@stu.omu.edu.tr>
2023-05-08 13:11:13 +0300

    add index.js

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
-
+$(document).ready(function() {
+  // Load Data butonuna tıklandığında
+  $("#load-btn").click(function() {
+    // Ajax isteğini gerçekleştir
+    $.ajax({
+      url: "data.json", // verilerin bulunduğu dosya yolu
+      type: "GET", // verileri çekmek için HTTP GET isteği gönder
+      dataType: "json", // verilerin tipi JSON olduğu için belirtiyoruz
+      success: function(data) { // istek başarılı olursa
+        // Verileri göstermek için HTML içeriğini oluştur
+        var html = "";
+        $.each(data, function(key, value) {
+          html += "<p>" + key + ": " + value + "</p>";
+        });
+        // HTML içeriğini data-container'a ekle
+        $("#data-container").html(html);
+      },
+      error: function() { // istek başarısız olursa
+        alert("Veriler yüklenemedi.");
+      }
+    });
+  });
+});
 
